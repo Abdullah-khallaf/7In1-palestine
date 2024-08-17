a2ac37801fe3f1890d8ee1f41b2b5d47b905db3a
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2024-08-17 21:42:16 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<!-- src/App.vue -->
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <Sidebar />
+      </div>
+      <div class="col-md-9">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style>
+.container-fluid {
+  padding-top: 20px;
+}
+
+@media (max-width: 768px) {
+  .col-md-3 {
+    display: none;
+  }
+
+  .col-md-9 {
+    width: 100%;
+  }
+}
+</style>
